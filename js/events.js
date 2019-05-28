a5f3d115b8da60fd029738e57ee7256b279e4f5f
Desalegn Berihun <desunew@gmail.com>
2019-05-28 03:04:15 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,19 @@
 //define functions here
+$('p').on('click', function getIt(){
+  
+  alert('Hey!')
+});
+
+$('.tasty').on('load', function frameIt(){
+  
+  
+});
+$(document).on('keydown', function(){
+  
+  if (key.which ==71){
+    alert('letter G is pressed')
+  }
+});
 
 $(document).ready(function(){
 
